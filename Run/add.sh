ad1a7e61c7d0f09d32bd8f4e85838bbeb2be3e8e
brunoterkaly <brunoterkaly@hotmail.com>
2017-06-09 07:53:46 +0000

    first commit

--- Run/add.sh
+++ Run/add.sh
@@ -1,6 +1,6 @@
-curl -i -H "Content-Type: application/json" -X POST -d '{"uid": "1", "coursenumber" : "401" , "coursetitle" : "K8s", "notes" : "An orchestrator"}' http://52.173.130.54/courses/add
+curl -i -H "Content-Type: application/json" -X POST -d '{"uid": "1", "coursenumber" : "401" , "coursetitle" : "K8s", "notes" : "An orchestrator"}' http://52.173.89.129/courses/add
 
-curl -i -H "Content-Type: application/json" -X POST -d '{"uid": "2", "coursenumber" : "402" , "coursetitle" : "DC/OS", "notes" : "for big workloads"}' http://52.173.130.54/courses/add
+curl -i -H "Content-Type: application/json" -X POST -d '{"uid": "2", "coursenumber" : "402" , "coursetitle" : "DC/OS", "notes" : "for big workloads"}' http://52.173.89.129/courses/add
 
-curl -i -H "Content-Type: application/json" -X POST -d '{"uid": "3", "coursenumber" : "403" , "coursetitle" : "Docker Swarm", "notes" : "Easy to use"}' http://52.173.130.54/courses/add
+curl -i -H "Content-Type: application/json" -X POST -d '{"uid": "3", "coursenumber" : "403" , "coursetitle" : "Docker Swarm", "notes" : "Easy to use"}' http://52.173.89.129/courses/add
 
--- Run/query.sh
+++ Run/query.sh
@@ -1,4 +1,4 @@
-curl http://52.173.130.54/courses/1
-curl http://52.173.130.54/courses/2
-curl http://52.173.130.54/courses/3
+curl http://52.173.89.129/courses/1
+curl http://52.173.89.129/courses/2
+curl http://52.173.89.129/courses/3
 
